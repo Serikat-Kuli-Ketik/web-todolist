24046b16ee50832b3840fee981e67f55cd40424c
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-17 09:56:18 +0700

    feat: add new task

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import { useState } from "react";
-import styles from "../styles/Home.module.css";
+import { FormEventHandler, useState } from "react";
+import styled from "styled-components";
 
 type Task = {
   id: string;
@@ -25,26 +25,95 @@ const initialTasks: Task[] = [
   },
 ];
 
+const MainContainer = styled.div`
+  width: 100vh;
+  height: fit-content;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /* background-color: red; */
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-family: sans-serif;
+`;
+
+const TasksContainer = styled.ul`
+  width: 100%;
+  padding: 0;
+`;
+
+const Task = styled.li`
+  display: block;
+  width: 100%;
+  padding: 10px 15px;
+  background-color: lightgrey;
+  margin: 10px 5px;
+  border-radius: 5px;
+`;
+
+const NewTaskForm = styled.form`
+  align-self: flex-start;
+`;
+
+const NewTaskInput = styled.input`
+  margin: 0 10px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid grey;
+`;
+
+const NewTaskSubmit = styled.input`
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid lightgrey;
+`;
+
 export default function Home() {
   const [tasks, setTasks] = useState(initialTasks);
+  const [newTask, setNewTask] = useState<string>("");
+
+  const onNewTaskSave: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (newTask === undefined || newTask === "") return;
+    setTasks([...tasks, { id: "hehe", title: newTask }]);
+    setNewTask("");
+  };
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Todo List</title>
         <meta name="description" content="Cross-platform todo list app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Tasks</h1>
-        <ul>
+
+      <MainContainer>
+        <Title>Tasks</Title>
+
+        <TasksContainer>
           {tasks.map((task, idx) => (
-            <li key={idx} className={styles.task}>
-              {task.title}
-            </li>
+            <Task key={idx}>{task.title}</Task>
           ))}
-        </ul>
-      </main>
+        </TasksContainer>
+
+        <NewTaskForm onSubmit={onNewTaskSave}>
+          <NewTaskInput
+            type="text"
+            placeholder="Insert new task"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <NewTaskSubmit type="submit" value="Save" />
+        </NewTaskForm>
+      </MainContainer>
     </div>
   );
 }
