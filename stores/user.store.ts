e2708ce25468117fc4317ef83dd54880108fcd7e
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-23 08:22:42 +0700

    feat: integrate userStore with localStorage

--- stores/user.store.ts
+++ stores/user.store.ts
@@ -1,11 +1,22 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
+import { LocalStorageItems } from "../shared/constants";
 
 type UserState = {
   userId: string | null;
   set: (userId: string) => void;
+  delete: () => void;
 };
 
-export const useUserStore = create<UserState>()((set) => ({
-  userId: null,
-  set: (userId) => set({ userId }),
-}));
+export const useUserStore = create<UserState>()(
+  persist(
+    (set, get) => ({
+      userId: null,
+      set: (userId) => set({ userId }),
+      delete: () => set({ userId: null }),
+    }),
+    {
+      name: LocalStorageItems.USER,
+    }
+  )
+);
