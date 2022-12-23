deca20a9f9154c8ff205f478ee7a145cdbd8bd8b
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-23 20:07:45 +0700

    feat: add error handling in swr fetcher

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,26 @@
-export const swrFetcher = (...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((res) => res.json());
+export const swrFetcher = async (...args: Parameters<typeof fetch>) => {
+  const res = await fetch(...args);
+
+  if (!res.ok) {
+    const error = new SwrFetcherError({
+      message: "An error occured while fetching the data",
+      info: await res.json(),
+      status: res.status,
+    });
+
+    throw error;
+  }
+
+  return await res.json();
+};
+
+class SwrFetcherError extends Error {
+  info;
+  status;
+  constructor(params: { message: string; info: object; status: number }) {
+    const { message, info, status } = params;
+    super(message);
+    this.info = info;
+    this.status = status;
+  }
+}
