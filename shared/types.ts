85c8897f633754f906611f32a855a686ef2eaf94
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-24 13:41:45 +0700

    feat: add status to Task type

--- shared/types.ts
+++ shared/types.ts
@@ -1,7 +1,14 @@
+export enum TaskStatus {
+  OPEN = "Open",
+  IN_PROGRESS = "In Progress",
+  COMPLETED = "Completed",
+}
+
 export type Task = {
   id: string;
   title: string;
   content?: string;
+  status: TaskStatus;
   labels: Array<{ id: string; title: string; color: string }>;
   reminders: Array<{
     id: string;
