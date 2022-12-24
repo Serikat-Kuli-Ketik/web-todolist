e63aa5754526c511d33f09493955a87c42fd480f
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-24 10:20:02 +0700

    feat(task detail page): display content, labels, reminders, and task status

--- pages/tasks/[task-id].tsx
+++ pages/tasks/[task-id].tsx
@@ -5,12 +5,9 @@ import useSwr from "swr";
 import { swrFetcher } from "../../utils";
 import { useRouter } from "next/router";
 import { Loading } from "../../components/loading";
-
-type Task = {
-  id: string;
-  title: string;
-  content?: string;
-};
+import { format } from "date-fns";
+import { Alarm, Repeat } from "tabler-icons-react";
+import { Task } from "../../shared/types";
 
 export default function TaskDetail() {
   useRouteProtection();
@@ -37,29 +34,117 @@ export default function TaskDetail() {
       </Head>
 
       <MainContainer>
-        <Title>Tasks</Title>
-
-        <TasksContainer>
-          <Task>{data?.data.title}</Task>
-        </TasksContainer>
+        <TitleContainer>
+          <Title>{data?.data.title}</Title>
+          <TaskStatusSelector name="task-status" id="task-status-selector">
+            <option value="open">Open</option>
+            <option value="open">In Progress</option>
+            <option value="open">Finished</option>
+          </TaskStatusSelector>
+        </TitleContainer>
+        <TaskContentContainer>
+          <div id="content-container">
+            <p className="section-title">Content</p>
+            <p>{data?.data.content}</p>
+          </div>
+          <div id="labels-container">
+            <p className="section-title">Labels</p>
+            <ul>
+              {data?.data.labels.map((label, idx) => (
+                <TaskLabel key={idx} color={label.color}>
+                  {label.title}
+                </TaskLabel>
+              ))}
+            </ul>
+          </div>
+          <div id="reminders-container">
+            <p className="section-title">Reminders</p>
+            <TaskReminderListContainer id="reminders-list-container">
+              {data?.data.reminders.map((reminder, idx) => (
+                <TaskReminder key={idx}>
+                  <div>
+                    <Alarm id="task-alarm-icon" />
+                    <div>
+                      <p>
+                        {format(new Date(reminder.datetime), "dd MMM yyyy")}
+                      </p>
+                      <p>{format(new Date(reminder.datetime), "HH:mm")}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <p>
+                      <Repeat size={10} /> {reminder.repeat} times every{" "}
+                      {reminder.interval}s
+                    </p>
+                  </div>
+                </TaskReminder>
+              ))}
+            </TaskReminderListContainer>
+          </div>
+        </TaskContentContainer>
       </MainContainer>
     </div>
   );
 }
 
 const MainContainer = styled.div`
-  width: 100vh;
+  width: 50vw;
   height: fit-content;
   padding: 20px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  margin: 0 auto;
+`;
+
+const TaskContentContainer = styled.div`
+  display: grid;
+  border-radius: 8px;
+  border: 0.5px solid lightgrey;
+  width: 100%;
+  min-height: 300px;
+  height: fit-content;
+  background-color: #f5f5f5;
+  padding: 20px;
+  grid-template-columns: 55% 45%;
+  grid-template-areas:
+    "content content"
+    "labels reminders";
+  gap: 10px;
+
+  #content-container {
+    grid-area: content;
+    min-height: 200px;
+  }
+
+  #labels-container {
+    grid-area: labels;
+    height: 100%;
+    width: 100%;
+  }
+
+  #reminders-container {
+    grid-area: reminders;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .section-title {
+    border-bottom: 1px solid lightgrey;
+    font-size: 0.9rem;
+    font-weight: bold;
+    color: #3a3b3c;
+    padding-bottom: 5px;
+    margin-bottom: 10px;
+  }
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
   align-items: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  /* background-color: red; */
+  margin: 20px 0;
 `;
 
 const Title = styled.h1`
@@ -67,16 +152,63 @@ const Title = styled.h1`
   font-family: sans-serif;
 `;
 
-const TasksContainer = styled.ul`
-  width: 100%;
-  padding: 0;
+const TaskStatusSelector = styled.select`
+  padding: 5px 10px;
+  box-sizing: border-box;
+  border-radius: 20px;
+  color: black;
 `;
 
-const Task = styled.li`
-  display: block;
-  width: 100%;
-  padding: 10px 15px;
-  background-color: lightgrey;
-  margin: 10px 5px;
+type TaskLabelProp = {
+  color: string;
+};
+
+const TaskLabel = styled.li<TaskLabelProp>`
+  display: inline-block;
+  font-size: 0.75rem;
+  padding: 5px 10px;
+  background-color: ${(prop) => prop.color};
+  mix-blend-mode: difference;
+  margin: 5px;
   border-radius: 5px;
+`;
+
+const TaskReminderListContainer = styled.div`
+  max-height: 150px;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TaskReminder = styled.div`
+  display: inline-grid;
+  width: 80%;
+  border-radius: 8px;
+  background-color: #dcdcdc;
+  padding: 10px;
+  font-size: 0.65rem;
+  align-items: center;
+  grid-template-columns: 60% 10% 30%;
+  grid-template-areas: "reminder-date . repeat-notice";
+  margin: 5px 0;
+
+  p {
+    margin: 5px 0;
+  }
+
+  #task-alarm-icon {
+    margin: 0 10px;
+  }
+
+  div:first-child {
+    display: inline-flex;
+    align-items: center;
+    grid-area: reminder-date;
+  }
+
+  > div:nth-child(2) {
+    font-size: 0.6rem;
+    grid-area: repeat-notice;
+  }
 `;
