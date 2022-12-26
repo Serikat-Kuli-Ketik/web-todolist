ce83eddb08557887fadb4da56833036779a1abad
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-26 13:53:19 +0700

    chore: setup react-modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import type { AppProps } from "next/app";
+import Modal from "react-modal";
+
+Modal.setAppElement("#__next");
 
 if (process.env.NODE_ENV === "development") {
   require("../mocks");
